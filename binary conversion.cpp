f0cda88376cb8bbed7ac511c637b8b571d229807
harsh-poriyaalar <101333149+harsh-poriyaalar@users.noreply.github.com>
2022-03-11 19:26:33 +0530

    init

---
+++ binary conversion.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n, target, q, r, count = 0;
+	int binary[count];
+	//int *p;
+	//p = binary;
+	printf("Enter decimal: ");
+	scanf("%d", &n);
+	target = n;
+	while(target != 0)
+	{
+	r = target%2;
+	binary[count]=r;
+    //*p = r;
+    count++;
+    //p++;
+ 	q = (target-r)/2;
+	target = q;
+    }
+    printf("%d in binary: ",n);
+	for(int i = count-1; i >= 0; i--)
+	{
+		printf("%d", binary[i]);
+	}
+}
